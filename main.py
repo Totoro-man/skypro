308ceb3452218a1997c204fbfd41f27de34a3c59
Тоторо <roman.aka.tod@gmail.com>
2023-12-06 02:58:56 +0300

    json from file

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from dlls import *
 JSON_URL = "https://www.jsonkeeper.com/b/PLZ0"
 JSON_PATH = "data/operations"
 
-operation_list = jsonData.get_list_from_url(JSON_URL)
+operation_list = jsonData.get_list_from_file(JSON_PATH)
 operation_list = utils.get_executed_receipts(operation_list)
-operation_list = utils.get_latest_receipts(5, operation_list)
+operation_list = utils.get_latest_receipts(3, operation_list)
 
 for i in operation_list:
     print(utils.get_formatted_receipt(i))
