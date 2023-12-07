40705eccb0394d2fbe6f18348a81106efaceb087
Тоторо <roman.aka.tod@gmail.com>
2023-12-07 04:52:27 +0300

    tests added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ JSON_PATH = "data/operations"
 
 operation_list = jsonData.get_list_from_file(JSON_PATH)
 operation_list = utils.get_executed_receipts(operation_list)
-operation_list = utils.get_latest_receipts(3, operation_list)
+operation_list = utils.get_latest_receipts(5, operation_list)
 
 for i in operation_list:
     print(utils.get_formatted_receipt(i))
